89a2196742997e5c02d765c79b6f1e2051852d3d
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-25 10:11:51 -0700

    Create convert.c

---
+++ convert.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <stddef.h>
+#include <string.h>
+#include "main.h"
+/**
+ * convert - Build out the printf function
+ * num: string passed with possible format specifiers
+ * base: base of conversion
+ * Return: number of characters printed
+ */
+char *convert (unsigned int num, int base)
+{
+    static char Representation[] = "0123456789ABCDEF";
+    static char buffer[50];
+    char *ptr;
+    ptr = &buffer[49];
+    *ptr = '\0';
+    do
+    {
+        *--ptr = Representation[num % base];
+        num /= base;
+    }
+    while (num != 0);
+    return (ptr);
+}
