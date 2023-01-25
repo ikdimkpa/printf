f47aa9f94f24fdee4355b4cf8c3c39392f6f2792
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-25 11:09:46 -0700

    Update convert.c

--- convert.c
+++ convert.c
@@ -10,7 +10,22 @@
  * base: base of conversion
  * Return: number of characters printed
  */
-char *convert (unsigned int num, int base)
+char *convert(unsigned int num, int base)
+{
+    static char Representation[] = "0123456789abcdef";
+    static char buffer[50];
+    char *ptr;
+    ptr = &buffer[49];
+    *ptr = '\0';
+    do
+    {
+        *--ptr = Representation[num % base];
+        num /= base;
+    }
+    while (num != 0);
+    return (ptr);
+}
+char *converts(unsigned int num, int base)
 {
     static char Representation[] = "0123456789ABCDEF";
     static char buffer[50];
