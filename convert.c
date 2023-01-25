f51441703c92d8a71bf234c71609593535355a26
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-25 11:11:37 -0700

    Update convert.c

--- convert.c
+++ convert.c
@@ -12,31 +12,37 @@
  */
 char *convert(unsigned int num, int base)
 {
-    static char Representation[] = "0123456789abcdef";
-    static char buffer[50];
-    char *ptr;
-    ptr = &buffer[49];
-    *ptr = '\0';
-    do
-    {
-        *--ptr = Representation[num % base];
-        num /= base;
-    }
-    while (num != 0);
-    return (ptr);
+	static char Representation[] = "0123456789abcdef";
+	static char buffer[50];
+	char *ptr;
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do
+	{
+		*--ptr = Representation[num % base];
+		num /= base;
+	}
+	while (num != 0);
+	return (ptr);
 }
+/**
+ * converts - Build out the printf function
+ * num: string passed with possible format specifiers
+ * base: base of conversion
+ * Return: number of characters printed
+ */
 char *converts(unsigned int num, int base)
 {
-    static char Representation[] = "0123456789ABCDEF";
-    static char buffer[50];
-    char *ptr;
-    ptr = &buffer[49];
-    *ptr = '\0';
-    do
-    {
-        *--ptr = Representation[num % base];
-        num /= base;
-    }
-    while (num != 0);
-    return (ptr);
+	static char Representation[] = "0123456789ABCDEF";
+	static char buffer[50];
+	char *ptr;
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do
+	{
+		*--ptr = Representation[num % base];
+		num /= base;
+	}
+	while (num != 0);
+	return (ptr);
 }
