e4f4393515023ee4675098a9fabc3473f18e94bf
Satope Oladeji Paul <118179546+posatope@users.noreply.github.com>
2023-01-29 18:22:45 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -10,87 +10,78 @@
  */
 int _printf(const char *format, ...)
 {
-	int i, j;
+	int i, j, num;
 	char *strings, *rev;
 	va_list ap;
+	unsigned int hex;
 
 	i = j = 0;
 	va_start(ap, format);
 	while (format[i] != '\0')
 	{
-		if (format[i] == '%' && format[i + 1] == 'c')
-		{
-			putchar(va_arg(ap, int));
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 's')
-		{
-			strings = va_arg(ap, char *);
-			for (j = 0; strings[j] != '\0'; j++)
-				putchar(strings[j]);
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			putchar('%');
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 'r')
-		{
-			strings = va_arg(ap, char *);
-			rev = rev_string(strings);
-			for (j = 0; rev[j] != '\0'; j++)
-				putchar(rev[j]);
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 'd')
-		{
-			int num;
-			
-			num = va_arg(ap, int);
-			if (num < 0)
-			{
-				putchar('-');
-				fputs(convert(-num, 10), stdout);
-			}
-			else
-			{
-				fputs(convert(num, 10), stdout);
-			}
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 'i')
-		{
-			unsigned int num;
-			
-			num = va_arg(ap, unsigned int);
-			fputs(convert(num, 10), stdout);
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1] == 'x')
-		{
-			unsigned int hex;
-
-			hex = va_arg(ap, unsigned int);
-			fputs(convert(hex, 16), stdout);
-		}
-		else if (format[i] == '%' && format[i + 1] == 'X')
-		{
-			unsigned int hex;
-
-			hex = va_arg(ap, unsigned int);
-			fputs(converts(hex, 16), stdout);
-		}
-		else if (format[i] == '%' && format[i + 1] == 'o')
-		{
-			unsigned int oct;
-
-			oct = va_arg(ap, unsigned int);
-			fputs(convert(oct, 8), stdout);
-		}
-		else
-			putchar(format[i]);
-		i++;
+		if (format[i] == '%' 
+		    switch (format[i + 1]) {
+			    case 'c':
+			    {
+				    putchar(va_arg(ap, int));
+				    i++;
+			    }
+			    case 's':
+			    {
+				    strings = va_arg(ap, char *);
+				    for (j = 0; strings[j] != '\0'; j++)
+					    putchar(strings[j]);
+				    i++;
+			    }
+			    case '%':
+			    {
+				    putchar('%');
+				    i++;
+			    }
+			    case 'r':
+			    {
+				    strings = va_arg(ap, char *);
+				    rev = rev_string(strings);
+				    for (j = 0; rev[j] != '\0'; j++)
+					    putchar(rev[j]);
+				    i++;
+			    }
+			    case 'd':
+			    {
+				    num = va_arg(ap, int);
+				    if (num < 0)
+				    {
+					    putchar('-');
+					    fputs(convert(-num, 10), stdout);
+				    }
+				    else
+				    {
+					    fputs(convert(num, 10), stdout);
+				    }
+			    }
+			    case 'i':
+			    {
+				    hex = va_arg(ap, unsigned int);
+				    fputs(convert(hex, 10), stdout);
+			    }
+			    case 'x':
+			    {
+				    hex = va_arg(ap, unsigned int);
+				    fputs(convert(hex, 16), stdout);
+			    }
+			    case 'X':
+			    {
+				    hex = va_arg(ap, unsigned int);
+				    fputs(converts(hex, 16), stdout);
+			    }
+			    case 'o':
+			    {
+				    hex = va_arg(ap, unsigned int);
+				    fputs(convert(hex, 8), stdout);
+			    }
+			    else
+				    putchar(format[i]);
+			    i++;
 	}
 	va_end(ap);
 	return (0);
